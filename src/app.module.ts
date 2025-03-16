21204551d4df0445677fa211675855cf8668282a
Ruslan Sattarov <r8sattarov@mail.ru>
2025-03-16 16:08:31 +0500

    feat: Implement prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DbService } from './db/db.service';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule],
   controllers: [AppController],
-  providers: [AppService, DbService]
+  providers: [AppService]
 })
 export class AppModule {}
